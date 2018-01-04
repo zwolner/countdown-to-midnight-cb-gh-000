a4ac91b24861fdf4e4a47e35fa18276489867021
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 19:07:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
-require 'pry'
+
 
 def countdown(number)
   while number >= 0
     puts "#{number} SECOND(S)"
     number -= 1
-    break.pry
   end
 
   return "HAPPY NEW YEAR!"
