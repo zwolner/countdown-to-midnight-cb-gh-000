22c0ef55e7502014bcb0d04e55678b66d174c737
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 19:12:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    countdown_with_sleep(number/number)
+    sleep(1)
   end
 
   return "HAPPY NEW YEAR!"
