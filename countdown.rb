d0ae2ba82dbb6c767ceafda40538cd2be848a7da
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 18:55:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,11 @@
 
 def countdown(number)
   while number>=0
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     number -= 1
     countdown_with_sleep(number/number)
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(x)
