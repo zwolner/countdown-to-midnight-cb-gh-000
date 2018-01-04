6ce7b2f7a51547e7c6c3215714a3412d90112f0c
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 19:08:19 +0000

    automatically backed up by learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -4,7 +4,7 @@ require 'benchmark'
 describe 'LOOPS' do
 
   describe '#countdown' do
-    let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
+    let(:countdown_output) { "10 SECOND(S)!\9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
 
     it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
       expect { countdown(10) }.to output(countdown_output).to_stdout
